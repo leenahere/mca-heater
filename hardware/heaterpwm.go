8c1f433fd2c293c9756a98111ce2c930f076343d
Lina <lina.pete@googlemail.com>
2019-02-27 14:57:10 +0100

    first attempt to connect spi and pwm to Api

--- hardware/heaterpwm.go
+++ hardware/heaterpwm.go
@@ -1,34 +1,46 @@
 package main
 
 import (
-        "os"
-        "time"
-        "github.com/stianeikeland/go-rpio"
+	"github.com/stianeikeland/go-rpio"
+	"os"
+	"time"
 )
 
+// postNewTempData posts the measured temperature to the restful API. Currently i should be the Volt measured.
+//TODO: Function for temperature probably needs some adjustment, depending on data type of i
+func getNewTargetData(i) {
+	response, err := http.Get("http://localhost:3000/heater")
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
+}
+
 func main() {
-        err := rpio.Open()
-        if err != nil {
-                os.Exit(1)
-        }
-        defer rpio.Close()
+	err := rpio.Open()
+	if err != nil {
+		os.Exit(1)
+	}
+	defer rpio.Close()
 
-        pin := rpio.Pin(18)
-        pin.Mode(rpio.Pwm)
-        pin.Freq(64000)
-        pin.DutyCycle(0, 256)
-        // the LED will be blinking at 2000Hz
-        // (source frequency divided by cycle length => 64000/32 = 2000)
+	pin := rpio.Pin(18)
+	pin.Mode(rpio.Pwm)
+	pin.Freq(64000)
+	pin.DutyCycle(0, 256)
+	// the LED will be blinking at 2000Hz
+	// (source frequency divided by cycle length => 64000/32 = 2000)
 
-        // five times smoothly fade in and out
-        for true {
-                for i := uint32(0); i < 256; i++ { // increasing brightness
-                        pin.DutyCycle(i, 256)
-                        time.Sleep(time.Second/256)
-                }
-                for i := uint32(256); i > 0; i-- { // decreasing brightness
-                        pin.DutyCycle(i, 256)
-                        time.Sleep(time.Second/256)
-                }
-        }
+	// five times smoothly fade in and out
+	for true {
+		for i := uint32(0); i < 256; i++ { // increasing brightness
+			pin.DutyCycle(i, 256)
+			time.Sleep(time.Second / 256)
+		}
+		for i := uint32(256); i > 0; i-- { // decreasing brightness
+			pin.DutyCycle(i, 256)
+			time.Sleep(time.Second / 256)
+		}
+	}
 }
--- hardware/heaterspi.go
+++ hardware/heaterspi.go
@@ -3,9 +3,30 @@ package main
 import (
   "log"
   "time"
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "strconv"
 
   rpio "github.com/stianeikeland/go-rpio"
 )
+
+// postNewTempData posts the measured temperature to the restful API. Currently i should be the Volt measured.
+//TODO: Function for temperature probably needs some adjustment, depending on data type of i
+func postNewTempData(i) {
+  var temperature = (((10000 / (15 - i)) * i) * (-0.003045)) + 55.45
+  jsonData := map[string]string{"temp": strconv.FormatFloat(temperature, 'f', 1, 64), "timestamp": strconv.FormatInt(time.Now().Unix(), 10)}
+  jsonValue, _ := json.Marshal(jsonData)
+  response, err = http.Post("http://localhost:3000/heater", "application/json", bytes.NewBuffer(jsonValue))
+  if err != nil {
+    fmt.Printf("The HTTP request failed with error %s\n", err)
+  } else {
+    data, _ := ioutil.ReadAll(response.Body)
+    fmt.Println(string(data))
+  }
+}
 
 func main() {
   if err := rpio.Open(); err != nil {
@@ -22,9 +43,11 @@ func main() {
 
   rpio.SpiSpeed(1000000)
   rpio.SpiChipSelect(0)
+  //TODO: Ticker needs to be adjusted. How often to we want to measure temp?
   ticker := time.NewTicker(time.Millisecond * 1000)
 
   //change 0 from range 0 to 7 to read from different channels
+  //TODO: Somewhere here postNewTempData needs to be called with correct spi value
   channel := byte(0)
   for range ticker.C {
     data := []byte{1, (8 + channel) << 4, 0}
